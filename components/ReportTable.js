cf52c900c4b399ab84560d009a3221d1e06ab3a6
Ricardo Soto-Fabela <sotofabela@gmail.com>
2023-02-09 23:08:36 -0600

    tried to add the login function

--- components/ReportTable.js
+++ components/ReportTable.js
@@ -57,3 +57,60 @@ export default function ResponseTable(props) {
     )
   );
 }
+
+
+// dont delete after this
+
+// function CookieStandTable({ stands, deleteStand }) {
+
+//   return (
+//       <table className="my-8">
+//           <thead>
+//               <tr>
+//                   <th>Location</th>
+//                   <th>6 am</th>
+//                   <th>7 am</th>
+//                   <th>8 am</th>
+//                   <th>9 am</th>
+//                   <th>10 am</th>
+//                   <th>11 am</th>
+//                   <th>12 pm</th>
+//                   <th>1 pm</th>
+//                   <th>2 pm</th>
+//                   <th>3 pm</th>
+//                   <th>4 pm</th>
+//                   <th>5 pm</th>
+//                   <th>6 pm</th>
+//                   <th>7 pm</th>
+//                   <th>totals</th>
+//               </tr>
+//           </thead>
+//           <tbody>
+//               {stands.map(stand => (
+
+//                   <CookieStandRow key={stand.id} info={stand} deleteStand={deleteStand} />
+//               ))}
+//           </tbody>
+//       </table>
+//   );
+// }
+
+// function CookieStandRow({ info, deleteStand }) {
+
+//   function clickHandler() {
+//       deleteStand(info.id);
+//   }
+
+//   if (info.hourly_sales.length == 0) {
+//       // bunk data
+//       info.hourly_sales = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+//   }
+
+//   return (
+//       <tr>
+//           <td>{info.location} <button onClick={clickHandler}>[x]</button></td>
+//           {info.hourly_sales.map((slot,index) => <td key={index}>{slot}</td>)}
+//           <td>{info.hourly_sales.reduce((num, sum) => num + sum, 0)}</td>
+//       </tr>
+//   );
+// }
